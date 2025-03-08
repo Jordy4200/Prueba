c06d98fd892ae345bae43044e92cbdf3da24018b
Jordy4200 <jordyvelasco3322@gmail.com>
2025-03-07 21:10:12 -0500

    si

--- obj/Debug/net8.0/Dogs.AssemblyInfo.cs
+++ obj/Debug/net8.0/Dogs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dogs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9516f046b71534745119cc1aebd0e6fdc132de54")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dogs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dogs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
